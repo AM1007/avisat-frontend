d686f903b2aeb0c6e1371aca848a024bc9dcd3df
Andrew Motko <andrew.motko@gmail.com>
2025-05-16 23:46:39 +0300

    add: sections

---
+++ src/components/ui/ArticleCard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function ArticleCard({ title, image, url }) {
+  return (
+    <div className="group">
+      <div className="relative h-64 overflow-hidden">
+        <Image src={image} alt={title} fill className="object-cover" />
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+          <h3 className="text-white text-xl font-bold text-center mb-4">
+            {title}
+          </h3>
+          <Link
+            href={url}
+            className="px-4 py-2 bg-red-600 text-white hover:bg-red-700 transition-colors"
+          >
+            ДЕТАЛЬНІШЕ
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
